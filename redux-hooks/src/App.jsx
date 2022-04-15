51a58574743136e07468cfb45193c0b4a9f83c5a
Saravanan <sarvan.m20@gmail.com>
2022-04-15 20:20:33 +0530

    1st commit

---
+++ redux-hooks/src/App.jsx
@@ -0,0 +1,31 @@
+import { Children, useState } from "react";
+import "./App.css";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+
+const PrivateRoute = ({ isAuthenticated, children }) => {
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  const isAuthenticated = true; // this needs to update from the redux part
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-hooks/src/Redux/Login/reducer.js
@@ -0,0 +1,10 @@
+import { LOGIN_LOADING, LOGIN_SUCCESS, LOGIN_FAILURE } from "./action";
+
+const initState = {
+    username: "",
+    token: "",
+    loading: false,
+    error: false,
+    isAuthenticated: false
+}
+

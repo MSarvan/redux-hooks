612eac9fe379b1709425b39515e75b14b429b316
Saravanan <sarvan.m20@gmail.com>
2022-04-16 10:32:45 +0530

    login updated

--- redux-hooks/src/Components/Login.jsx
+++ redux-hooks/src/Components/Login.jsx
@@ -1,12 +1,37 @@
-
-
+import { useState } from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+      const userdata = {username, password};
+      fetch(`http://localhost:3000`)
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="User name"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <br />
+      <button onClick={handleLogin}>LOGIN</button>
+    </div>
+  );
+};
 
 export default Login;
--- redux-hooks/src/Redux/Login/reducer.js
+++ redux-hooks/src/Redux/Login/reducer.js
@@ -11,6 +11,23 @@ const initState = {
 const LoginReducer = (state = initState, {type, payload}) => { // action destructured as type, payload
     switch(type)
     {
-        case 
+        case LOGIN_LOADING:
+            return { ...state, loading: true }
+        case LOGIN_SUCCESS:
+            return { ...state,  username: payload.username,
+            token: payload.token,
+            loading: false,
+            error: false,
+            isAuthenticated: true }
+        case LOGIN_FAILURE:
+            return { ...state, username: "",
+            token: "",
+            loading: false,
+            error: true,
+            isAuthenticated: false }
+        default:
+            throw error;
     }
+
+    
 }
